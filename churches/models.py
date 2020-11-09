24fce5e6800c4801bdd877c91297f555c26a514a
Daniel Braun <daniel@pomas.ca>
2020-11-09 12:19:40 -0500

    added initial membership models

--- churches/models.py
+++ churches/models.py
@@ -36,3 +36,59 @@ class Church(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Person(models.Model):
+
+    class Gender(models.TextChoices):
+        MALE = 'male', 'Male'
+        FEMALE = 'female', 'Female'
+
+    class MaritalStatus(models.IntegerChoices):
+        SINGLE = 1, 'Single'
+        MARRIED = 2, 'Married'
+        WIDOWED = 3, 'Widowed'
+        DIVORCED = 4, 'Divorced'
+
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=100)
+    second_last_name = models.CharField(max_length=100, null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=Gender.choices)
+    date_of_birth = models.DateField(blank=True, null=True)
+    contact_number = models.CharField(max_length=30, null=True, blank=True)
+    marital_status = models.IntegerField(MaritalStatus.choices)
+    # Address
+    street_address = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    state = models.CharField(max_length=50)
+    Country = CountryField()
+
+    child_of = models.ForeignKey('Family', on_delete=models.PROTECT, related_name='children')
+
+    def __str__(self):
+        if self.second_last_name:
+            return f"{self.first_name} {self.last_name} {self.second_last_name}"
+        else:
+            return f"{self.first_name} {self.last_name}"
+
+
+class Family(models.Model):
+    father = models.OneToOneField('Person', on_delete=models.PROTECT, related_name='father_of')
+    mother = models.OneToOneField('Person', on_delete=models.PROTECT, related_name='mother_of')
+
+    def __str__(self):
+        return f"{self.father.first_name} & {self.mother.first_name} {self.father.last_name}"
+
+
+class Membership(models.Model):
+    member = models.ForeignKey('Person', on_delete=models.PROTECT, related_name='membership')
+    church = models.ForeignKey('Church', on_delete=models.PROTECT, related_name='memberships')
+    start_date = models.DateField(null=True, blank=True)
+    reason_for_joining = models.TextField(null=True, blank=True)
+    biblical_verse = models.TextField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    reason_for_leaving = models.TextField(null=True, blank=True)
+    is_member = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.church} - {self.member}"
